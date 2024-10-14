f1854fca21b9f6686dfcd004a729691b9b7fe2b3
muhamad afat <>
2024-10-14 18:29:48 +0700

    Video #8 Database Wrapper

---
+++ mvc/app/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('BASEURL', 'http://localhost/PrakWeb2024_A_223040014_PW/mvc/public');
+
+///DB
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
+?>
--- mvc/app/core/Constants.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-define('BASEURL', 'http://localhost/PrakWeb2024_A_223040014_PW/mvc/public');
-
-?>
