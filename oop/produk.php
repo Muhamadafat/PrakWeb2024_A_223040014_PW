23202a80b663e80b7744e27ff09daab75218617b
muhamad afat <>
2024-10-28 05:51:45 +0700

    Video #3 Property & Method

--- oop/produk.php
+++ oop/produk.php
@@ -1,7 +1,45 @@
 <?php
+
 //Jualan produk
 //komik
 //game
-class produk {}
-$produk1 = new produk();
-$produk2 = new produk();
+
+class produk
+{
+    public $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0;
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+//$produk1 = new produk("naruto");
+//$produk1->judul = "Naruto";
+//var_dump($produk1);
+//
+//$produk2 = new produk("Uncharted");
+//$produk2->judul = "Uncharted";
+//$produk2->tambahProperty = "Hahahhaa";
+//var_dump($produk2);
+
+$produk3 = new produk();
+$produk3->judul = "One Piece";
+$produk3->penulis = "Eiichiro Oda";
+$produk3->penerbit = "Shueisha";
+$produk3->harga = 50000;
+
+
+$produk4 = new produk();
+$produk4->judul = "Moon Knight: Fist of Khonshu (2024) #1";
+$produk4->penulis = "Jed Mackay";
+$produk4->penerbit = "Alessandro Cappuccio";
+$produk4->harga = 250000;
+
+echo "<br>";
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
