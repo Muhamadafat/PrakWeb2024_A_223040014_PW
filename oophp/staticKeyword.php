636f83284e5476de3e4e4d7bc6f9c51961f62714
muhamad afat <>
2024-11-01 04:28:02 +0700

    Video #11 Static Keyword

---
+++ oophp/staticKeyword.php
@@ -0,0 +1,27 @@
+<?php
+class Contoh
+{
+    public static $angka = 1;
+
+    public function halo()
+    {
+        return "Halo " . self::$angka++ . " kali";
+    }
+}
+
+$obj = new Contoh();
+echo $obj->halo();
+echo "<br>";
+echo $obj->halo();
+echo "<br>";
+echo "<hr>";
+$obj2 = new Contoh();
+echo $obj2->halo();
+echo "<br>";
+echo $obj2->halo();
+echo "<br>";
+$obj3 = new Contoh();
+echo $obj3->halo();
+echo "<br>";
+echo $obj2->halo();
+echo "<br>";
